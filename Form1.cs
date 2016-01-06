087636f3f81968b4c8ff38aab38058bca51cf07c
mattveal15 <m.veal2698@student.leedsbeckett.ac.uk>
2016-01-06 13:29:21 +0000

    First Commit
    Set up basics of the Bug Reporter. Made the initial home page that links
    to a bug reporter page. By the end of the project you will be able to
    add bugs as well as load previous bugs, which will be done via a link to
    a database

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugReporter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void informationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bug Reporting Program              \n\r\n\rLeeds Beckett University               \n\r\n\r(C) Matthew Veal");
+        }
+
+   
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReportBug frmchild = new ReportBug();
+            frmchild.MdiParent = this;
+            frmchild.Show();
+
+        }
+    }
+}
