c703ba75b319732e43db6a73061cd636ea050e52
mattveal15 <m.veal2698@student.leedsbeckett.ac.uk>
2016-01-07 15:55:14 +0000

    Commit 3
    Added a Login and Register form to the project now and changed the start
    up page so you have to login first.

---
+++ Register.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlServerCe;
+
+namespace Bug_Reporter
+{
+    public partial class Register : Form
+    {
+
+        SqlCeConnection mySqlConnection;
+
+        public Register()
+        {
+            InitializeComponent();
+            populateListBox();
+        }
+
+        public void populateListBox()
+        {
+            mySqlConnection = new SqlCeConnection(@"Data Source=C:\temp\Mydatabase.sdf ");
+
+            String selcmd = "SELECT user_name, password FROM users ORDER BY user_name";
+
+            SqlCeCommand mySqlCommand = new SqlCeCommand(selcmd, mySqlConnection);
+
+            try
+            {
+                mySqlConnection.Open();
+
+                SqlCeDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+            }
+
+            catch (SqlCeException ex)
+            {
+
+                MessageBox.Show("Failure" + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        public bool checkInputs()
+        {
+            bool rtnvalue = true;
+
+            if (string.IsNullOrEmpty(new_username.Text) ||
+                string.IsNullOrEmpty(new_password.Text))
+            {
+                MessageBox.Show("Error: Please check your inputs");
+                rtnvalue = false;
+            }
+
+            return (rtnvalue);
+
+        }
+
+        public void insertRecord(String user, String pass, String commandString)
+        {
+
+            try
+            {
+                SqlCeCommand cmdInsert = new SqlCeCommand(commandString, mySqlConnection);
+
+                cmdInsert.Parameters.AddWithValue("@user", user);
+                cmdInsert.Parameters.AddWithValue("@pass", pass);
+                cmdInsert.ExecuteNonQuery();
+            }
+            catch (SqlCeException ex)
+            {
+                MessageBox.Show(user + " .." + ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btn_register_Click(object sender, EventArgs e)
+        {
+            if (checkInputs())
+            {
+
+                String commandString = "INSERT INTO users(user_name, password) VALUES (@user, @pass)";
+
+                insertRecord(new_username.Text, new_password.Text, commandString);
+                populateListBox();
+                MessageBox.Show("Succesfully signed up, you can now log in!");
+
+                this.Hide();
+                Login frmchild = new Login();
+                frmchild.Show();
+            }
+        }
+                    
+    }
+}
